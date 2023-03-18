5ac70543326c50444d114c7e6deffc2df553afd6
snoopy267812 <124488728+snoopy267812@users.noreply.github.com>
2023-03-18 20:24:44 +0800

    Add files via upload

---
+++ ITSA7~14/ITSA_09/ITSA_09.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>  
+#include <math.h>  
+using namespace std;
+int main() {
+    int n = 0;
+    int ans = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) 
+    {
+        if (i % 3 == 0) { ans += i; }
+    }
+    cout << ans << '\n';
+    return 0;
+}
---
+++ ITSA7~14/ITSA_10/ITSA_10.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    while (cin >> a >> b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        cout << a << "\n";
+    }
+    return 0;
+}
