84737da98496350ace87e676883f0a1c0b018524
4B1G0110 <125246779+4B1G0110@users.noreply.github.com>
2023-03-19 12:20:16 +0800

    Add files via upload

---
+++ ITSA7~14/ITSA---第八題.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>  
+#include <stdio.h>  
+  
+int main(){  
+    int n,i;  
+    scanf("%d",&n);  
+    for(i=2;i<n;i++){  
+        if(n%i==0){  
+            break;  
+        }  
+    }  
+    if(i==n){  
+        printf("YES\n");  
+    }  
+    else{  
+        printf("NO\n");  
+    }  
+}  
